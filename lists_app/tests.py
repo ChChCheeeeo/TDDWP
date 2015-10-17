bb04a99dd0ee7640b912d6414b95837eb9551b8e
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-10-17 14:12:24 -0700

    Added test to check for title name and if response start with <html>

--- lists_app/tests.py
+++ lists_app/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists_app.views import home_page #1
 
 class HomePageTest(TestCase):
@@ -11,3 +12,25 @@ class HomePageTest(TestCase):
         # checking that resolve, when called with “/”, the root of the
         # site, finds a function called home_page. 
         self.assertEqual(found.func, home_page)  #3
+
+    def test_home_page_returns_correct_html(self):
+        # 1 create an HttpRequest object, which is what Django will
+        # see when a user’s browser asks for a page.
+        # 2 pass it to our home_page view, which gives us a response.
+        # You won’t be surprised to hear that this object is an
+        # instance of a class called HttpResponse. Then, we assert
+        # that the .content of the response—which is the HTML that we
+        # send to the user—has certain properties.
+        # We want it to start with an <html> tag which gets closed at
+        # the end. Notice that response.content is raw bytes, not a
+        # Python string, so we have to use the b'' syntax to compare
+        # them. More info is available in Django’s Porting to Python
+        # 3 docs.
+        # And we want a <title> tag somewhere in the middle, with the
+        # words "To-Do lists" in it—because that’s what we specified
+        # in our functional test. 
+        request = HttpRequest()  #1
+        response = home_page(request)  #2
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))
