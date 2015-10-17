d49e571f85d705c22625e6ba31ea1d3c9f387917
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-10-17 13:40:15 -0700

    Added new failing test to check for home page

--- lists_app/tests.py
+++ lists_app/tests.py
@@ -1,6 +1,13 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists_app.views import home_page #1
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  #2
+        # resolve is the function Django uses internally to resolve]
+        # URLs, and find what view function they should map to. We’re
+        # checking that resolve, when called with “/”, the root of the
+        # site, finds a function called home_page. 
+        self.assertEqual(found.func, home_page)  #3
