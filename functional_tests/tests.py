905a297d5dd23df67ac8d6da544759a180425316
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-10-21 09:24:28 -0700

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):#unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)#'http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -67,10 +68,10 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':  #7
-    # launches the unittest test runner, which will
-    # automatically find test classes and methods in the file
-    # and run them. 
-    # warnings='ignore' suppresses a superfluous ResourceWarning
-    # which was being emitted at the time of writing. 
-    unittest.main(warnings='ignore')  #8
+# if __name__ == '__main__':  #7
+#     # launches the unittest test runner, which will
+#     # automatically find test classes and methods in the file
+#     # and run them. 
+#     # warnings='ignore' suppresses a superfluous ResourceWarning
+#     # which was being emitted at the time of writing. 
+#     unittest.main(warnings='ignore')  #8
