e36e9703b135bed0c7ab85ff55779032cb51b6fd
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-11-04 09:41:26 -0800

    Unlike LiveServerTestCase, use StaticLiveServerTestCase because it can detect static files like runserver, FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):#unittest.TestCase):
+# class NewVisitorTest(LiveServerTestCase):#unittest.TestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
