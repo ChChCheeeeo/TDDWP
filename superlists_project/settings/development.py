659ca8aa0681eb49d09a4caf920fd056a4e756a6
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-11-18 10:39:05 -0800

    Set DEBUG varible for each file in settings before uploaded to DO

--- superlists_project/settings/development.py
+++ superlists_project/settings/development.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+# Needed when DEBUG=False
+# ALLOWED_HOSTS = ['tmpdomain-staging.tmpdomain.xyz']
