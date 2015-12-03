fba306b60c2f17b0bffb44199933504386290edb
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-12-02 20:40:35 -0800

    Build JavaScript unit test for desired functionality

---
+++ lists_app/static/list.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+    /*
+        find all the input elements, and for each of them, attach an even
+         listener which reacts on keypress events. The event listener is
+          the inline function, which hides all elements that have the class
+           .has-error.
+    */
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
+});
