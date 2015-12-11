b330e08f0a752439faa4066d91d322c5e7fb70f5
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-12-10 16:18:56 -0800

    Now have a working login view

--- accounts_app/tests/test_views.py
+++ accounts_app/tests/test_views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user_model, SESSION_KEY
 from django.test import TestCase
 from unittest.mock import patch
+# Here get_user_model from django.contrib.auth finds the project’s user model
+# and it works whether you’re using the standard user model or a custom one,
+User = get_user_model()
 
 class LoginViewTest(TestCase):
 
@@ -22,3 +26,25 @@ class LoginViewTest(TestCase):
         # Mocks can make assertions. In this case, they can check whether
         # they were called, and what with. 
         mock_authenticate.assert_called_once_with(assertion='assert this')
+    @patch('accounts_app.views.authenticate')
+    def test_returns_OK_when_user_found(
+        self, mock_authenticate
+    ):
+        user = User.objects.create(email='a@b.com')
+        user.backend = ''  # required for auth_login to work
+        mock_authenticate.return_value = user
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        # The Django test client keeps track of the session for its user.
+        # For the case where the user gets authenticated successfully, 
+        # check that their user ID (the primary key, or pk) is associated
+        # with their session. 
+        self.assertEqual(self.client.session[SESSION_KEY], str(user.pk))
+    @patch('accounts_app.views.authenticate')
+    def test_does_not_get_logged_in_if_authenticate_returns_None(
+        self, mock_authenticate
+    ):
+        mock_authenticate.return_value = None
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        # In the case where the user should not be authenticated,
+        # the SESSION_KEY should not appear in their session. 
+        self.assertNotIn(SESSION_KEY, self.client.session)
