ce18dfe882be6d5f7c1438d05435908f1bfe1f64
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-12-11 21:28:02 -0800

    Finish Off FT, test logout.

--- accounts_app/static/accounts.js
+++ accounts_app/static/accounts.js
@@ -8,6 +8,9 @@ var initialize = function (navigator, user, token, urls) {
     $('#id_login').on('click', function () {
         navigator.id.request();
     });
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
 
     navigator.id.watch({
       /*
@@ -28,7 +31,14 @@ var initialize = function (navigator, user, token, urls) {
           .done(function () { window.location.reload(); })
           .fail(function () { navigator.id.logout(); });
       },
-      onlogout: function () {}
+      onlogout: function () {
+          $.post(
+            urls.logout,
+            { csrfmiddlewaretoken: token}
+          )
+            .done(function () { window.location.reload(); })
+            .fail(function () { navigator.id.logout(); });
+      } // onlogout
     }); // navigator.id.watch
 };
 
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -4,6 +4,40 @@ import time
 
 class LoginTest(FunctionalTest):
 
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
+
+
+    def switch_to_new_window(self, text_in_title):
+        retries = 60
+        while retries > 0:
+            # looking for a particular title
+            for handle in self.browser.window_handles:
+                self.browser.switch_to_window(handle)
+                if text_in_title in self.browser.title:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail('could not find window')
+
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
+
+
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
@@ -29,26 +63,17 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        # this does some waiting
-        self.wait_for_element_with_id('id_logout')#logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.wait_to_be_logged_in()
 
-    def switch_to_new_window(self, text_in_title):
-        retries = 60
-        while retries > 0:
-            # looking for a particular title
-            for handle in self.browser.window_handles:
-                self.browser.switch_to_window(handle)
-                if text_in_title in self.browser.title:
-                    return
-            retries -= 1
-            time.sleep(0.5)
-        self.fail('could not find window')
+        # Refreshing the page, she sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
 
-    def wait_for_element_with_id(self, element_id):
-        # good for when waiting for an element with a given ID to
-        # appear on the page
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
-        )
+        # Terrified of this new feature, she reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The "logged out" status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
