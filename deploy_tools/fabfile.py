77d7ffca21c2e970eb5d88899f7adb491cf32d86
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-12-12 11:55:30 -0800

    Try to fix static dir path

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -54,8 +54,7 @@ def _get_latest_source(source_folder):
 def _update_settings(source_folder, site_name):
     # update the settings file, to set the ALLOWED_HOSTS and DEBUG,
     # and to create a new secret key
-    #settings_path = source_folder + '/superlists_project/settings/base.py'#settings.py'
-    settings_path = source_folder + '/settings/base.py'#settings.py'
+    settings_path = source_folder + '/superlists_project/settings/base.py'#settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
