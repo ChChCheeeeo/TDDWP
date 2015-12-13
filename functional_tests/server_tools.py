5f004c9f190d3bcf3f8126a9a004957137a008b2
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-12-12 18:45:49 -0800

    An Additional Hop via subprocess. an extra hop and call the fab command as a new process, like we do from the command line when we do server deploys.

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,25 @@
+from os import path
+import subprocess
+THIS_FOLDER = path.dirname(path.abspath(__file__))
+
+# use the subprocess module to call some Fabric functions using the fab command.
+def create_session_on_server(host, email):
+    return subprocess.check_output(
+        [
+            'fab',
+            # command-line syntax for arguments to fab functions
+            'create_session_on_server:email={}'.format(email),
+            '--host={}'.format(host),
+            '--hide=everything,status',
+        ],
+        cwd=THIS_FOLDER
+    #  be quite careful about extracting the session key as a string from the
+    # output of the command as it gets run on the server. 
+    ).decode().strip()
+
+
+def reset_database(host):
+    subprocess.check_call(
+        ['fab', 'reset_database', '--host={}'.format(host)],
+        cwd=THIS_FOLDER
+    )
